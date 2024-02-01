386b69dc87f966945f5fe3d47b5d5ab84800ae25
Zofia Stawarska <z.stawarska@gmail.com>
2024-02-01 23:24:08 +0100

    add App class component

--- src/App.js
+++ src/App.js
@@ -1,2 +1,31 @@
 import React from "react";
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { location : "Lisbon"};
+        this.fetchWeather = this.fetchWeather.bind(this);
+    }
+
+    fetchWeather() {
+        console.log(this);
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <h1>Classy Weather</h1>
+                <div>
+                    <input 
+                        type="text" 
+                        placeholder="Search for location..."
+                        value={this.state.location}
+                        onChange={e => this.setState({ location: e.target.value })} />
+                </div>
+                <button onClick={this.fetchWeather}>Get weather</button>
+            </div>
+        )
+    }
+}
+
+export default App;
